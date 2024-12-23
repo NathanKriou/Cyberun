1281a560b782831325d8dc691f17ffcdc9bfb783
Nathan Cariou <nathan.cariou@free.fr>
2024-12-23 02:16:58 +0000

    dz

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    loader: {
-      ".js": "jsx", // Traite les fichiers .js avec JSX
-      ".jsx": "jsx", // Traite les fichiers .jsx
-    },
-    include: /\.[jt]sx?$/, // Inclut les fichiers .ts, .tsx, .js, .jsx
-  },
-  css: {
-    preprocessorOptions: {
-      css: {
-        // Options pour les préprocesseurs CSS si nécessaire
-      },
-    },
+    loader: "jsx", // Traitement des fichiers .js et .jsx avec JSX
   },
 });
