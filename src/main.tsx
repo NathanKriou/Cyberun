7b55dfcd2b931519a11c3ed74cb592f937db383b
Nathan Cariou <nathan.cariou@free.fr>
2024-12-23 04:07:56 +0000

    extension tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx"; // Import sans l'extension .tsx
+import App from "./App.tsx"; // Utilisation de .tsx si ton fichier App est en TypeScript
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
