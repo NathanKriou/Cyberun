3ed34fd80a5f90641c6ee43938fe459f5c40e7c2
Nathan Cariou <nathan.cariou@free.fr>
2024-12-23 00:09:28 +0000

    main.tsx update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Vérification de l'existence de l'élément root avant de l'utiliser
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error('L\'élément avec id "root" est introuvable.');
+}
