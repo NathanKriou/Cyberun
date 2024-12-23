b5898b05617006cf0d98ee063ac2a812558e3cba
Nathan Cariou <nathan.cariou@free.fr>
2024-12-23 01:08:26 +0000

    erreurs deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App.tsx"; // Assurez-vous que l'extension .tsx est bien incluse
 
-// Vérification de l'existence de l'élément root avant de l'utiliser
 const rootElement = document.getElementById("root");
-
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <App />
     </StrictMode>
   );
-} else {
-  console.error('L\'élément avec id "root" est introuvable.');
 }
